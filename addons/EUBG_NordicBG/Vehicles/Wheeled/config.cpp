e132e3c260f3986d67ab184383c4f9614135d9ba
ancorb1974 <ancorb1974@live.nl>
2016-05-19 21:09:05 +0200

    added trucks

--- addons/EUBG_NordicBG/Vehicles/Wheeled/config.cpp
+++ addons/EUBG_NordicBG/Vehicles/Wheeled/config.cpp
@@ -16,6 +16,12 @@ class CfgVehicles
 	class B_MRAP_01_hmg_F;
 	class I_APC_Wheeled_03_cannon_F;
 	class B_APC_Wheeled_01_cannon_F;
+	class I_Truck_02_transport_F;
+	class I_Truck_02_covered_F;
+	class I_Truck_02_fuel_F;
+	class I_Truck_02_box_F;
+	class I_Truck_02_medical_F;
+	class I_Truck_02_ammo_F;
 
 	class EUBG_Nordic_S_PatgB203: O_APC_Wheeled_02_rcws_F
 	{
@@ -500,6 +506,402 @@ class CfgVehicles
 		hiddenSelections[] = {"Camo1", "Camo2","Camo3"};
 		hiddenSelectionsTextures[] = {"\EUBG_NordicBG\Vehicles\Wheeled\patriaAMV\patria_body_FIN_co.paa", "\EUBG_NordicBG\Vehicles\Wheeled\patriaAMV\patria_wheels_FIN_co.paa", "\EUBG_NordicBG\Vehicles\Wheeled\patriaAMV\patria_turret_FIN_co.paa"};
 
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class EUBG_Nordic_S_Scania_Transport: I_Truck_02_transport_F
+	{
+		dlc="NordicBG";
+		author = "OldDutchGamer";
+		displayName="Scania (Transport)";
+		side=1;
+		scope=2;
+		faction="EUBG_Nordic_BG";
+		vehicleClass="EUBG_Nordic_S_Wheeled";
+		crew = "EUBG_Nordic_S_GL";
+		typicalCargo[] = {"EUBG_Nordic_S_GL"};
+		hiddenSelectionsTextures[] = {"\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_front_co.paa","\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_covered_co.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class EUBG_Nordic_S_Scania_Transport_Covered: I_Truck_02_covered_F
+	{
+		dlc="NordicBG";
+		author = "OldDutchGamer";
+		displayName="Scania (covered_";
+		side=1;
+		scope=2;
+		faction="EUBG_Nordic_BG";
+		vehicleClass="EUBG_Nordic_S_Wheeled";
+		crew = "EUBG_Nordic_S_GL";
+		typicalCargo[] = {"EUBG_Nordic_S_GL"};
+		hiddenSelectionsTextures[] = {"\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_front_co.paa","\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_covered_co.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class EUBG_Nordic_S_Scania_Fuel: I_Truck_02_fuel_F
+	{
+		dlc="NordicBG";
+		author = "OldDutchGamer";
+		displayName="Scania (Fuel)";
+		side=1;
+		scope=2;
+		faction="EUBG_Nordic_BG";
+		vehicleClass="EUBG_Nordic_S_Wheeled";
+		crew = "EUBG_Nordic_S_GL";
+		typicalCargo[] = {"EUBG_Nordic_S_GL"};
+		hiddenSelectionsTextures[] = {"\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_front_co.paa","\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_fuel_co.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class EUBG_Nordic_S_Scania_repair: I_Truck_02_box_F
+	{
+		dlc="NordicBG";
+		author = "OldDutchGamer";
+		displayName="Scania (Repair)";
+		side=1;
+		scope=2;
+		faction="EUBG_Nordic_BG";
+		vehicleClass="EUBG_Nordic_S_Wheeled";
+		crew = "EUBG_Nordic_S_ENG";
+		typicalCargo[] = {"EUBG_Nordic_S_ENG"};
+		hiddenSelectionsTextures[] = {"\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_front_co.paa","\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_covered_co.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class EUBG_Nordic_S_Scania_ammo: I_Truck_02_ammo_F
+	{
+		dlc="NordicBG";
+		author = "OldDutchGamer";
+		displayName="Scania (Ammo)";
+		side=1;
+		scope=2;
+		faction="EUBG_Nordic_BG";
+		vehicleClass="EUBG_Nordic_S_Wheeled";
+		crew = "EUBG_Nordic_S_GL";
+		typicalCargo[] = {"EUBG_Nordic_S_GL"};
+		hiddenSelectionsTextures[] = {"\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_front_co.paa","\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_covered_co.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class EUBG_Nordic_S_Scania_medical: I_Truck_02_medical_F
+	{
+		dlc="NordicBG";
+		author = "OldDutchGamer";
+		displayName="Scania (Medical)";
+		side=1;
+		scope=2;
+		faction="EUBG_Nordic_BG";
+		vehicleClass="EUBG_Nordic_S_Wheeled";
+		crew = "EUBG_Nordic_S_Medic";
+		typicalCargo[] = {"EUBG_Nordic_S_Medic"};
+		hiddenSelectionsTextures[] = {"\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_front_co.paa","\EUBG_NordicBG\Vehicles\Wheeled\Scania\scania_covered_co.paa"};
+		
 		class TransportItems {
 			class _xx_FirstAidKit {
 				name = "FirstAidKit";
